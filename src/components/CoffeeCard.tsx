09d11560481b9ade9ae273d36993f9b1ef0473ca
vjdato21 <vjdato21@outlook.com>
2024-04-15 14:13:30 +0800

    Update CoffeeCard.tsx

--- src/components/CoffeeCard.tsx
+++ src/components/CoffeeCard.tsx
@@ -69,7 +69,7 @@ const CoffeeCard: React.FC<CoffeeCardProps> = ({
       <Text style={styles.CardSubtitle}>{special_ingredient}</Text>
       <View style={styles.CardFooterRow}>
         <Text style={styles.CardPriceCurrency}>
-          $ <Text style={styles.CardPrice}>{price.price}</Text>
+          ₱ <Text style={styles.CardPrice}>{price.price}</Text>
         </Text>
         <TouchableOpacity
           onPress={() => {
